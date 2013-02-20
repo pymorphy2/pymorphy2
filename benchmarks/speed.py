ed0c11034e49809331326b6ff1ab9cdb47ec4507
Mikhail Korobov <kmike84@gmail.com>
2013-02-20 19:53:16 +0600

    more benchmarks

--- benchmarks/speed.py
+++ benchmarks/speed.py
@@ -52,10 +52,10 @@ def bench_tag(morph, words, total_usages, repeats):
 
     measure = functools.partial(utils.measure, repeats=repeats)
 
-    logger.info("    morph.tag: %0.0f words/sec (with freq. info)", measure(_run, total_usages))
-    logger.info("    morph.tag: %0.0f words/sec (without freq. info)", measure(_run_nofreq, len(words)))
-    logger.info("    morph.tag: %0.0f words/sec (without freq. info, umlauts removed from input)", measure(_run_no_umlauts, len(words)))
-    logger.info("    morph.tag: %0.0f words/sec (without freq. info, str(tag) called)", measure(_run_str, len(words)))
+    logger.info("    morph.tag(w): %0.0f words/sec (considering word frequencies)", measure(_run, total_usages))
+    logger.info("    morph.tag(w): %0.0f words/sec", measure(_run_nofreq, len(words)))
+    logger.info("    morph.tag(w): %0.0f words/sec (umlauts removed from input)", measure(_run_no_umlauts, len(words)))
+    logger.info("    morph.tag(w): %0.0f words/sec (str(tag) called)", measure(_run_str, len(words)))
 
 
 def bench_parse(morph, words, total_usages, repeats):
@@ -68,10 +68,53 @@ def bench_parse(morph, words, total_usages, repeats):
         for word, cnt in words:
             morph.parse(word)
 
+    def _run_normal_form():
+        for word, cnt in words:
+            [p.normal_form for p in morph.parse(word)]
+
+    def _run_normalized():
+        for word, cnt in words:
+            [p.normalized for p in morph.parse(word)]
+
+    def _run_is_noun():
+        for word, cnt in words:
+            [set(['NOUN']) in p.tag for p in morph.parse(word)]
+
+    def _run_is_noun2():
+        for word, cnt in words:
+            [p.tag.POS == 'NOUN' for p in morph.parse(word)]
+
+    def _run_word_is_known():
+        for x in range(10):
+            for word, cnt in words:
+                morph.word_is_known(word)
+
+    def _run_lexeme():
+        for word, cnt in words[::5]:
+            [p.lexeme for p in morph.parse(word)]
+
     measure = functools.partial(utils.measure, repeats=repeats)
 
-    logger.info("    morph.parse: %0.0f words/sec (with freq. info)", measure(_run, total_usages))
-    logger.info("    morph.parse: %0.0f words/sec (without freq. info)", measure(_run_nofreq, len(words)))
+    def show_info(bench_name, func, note='', count=len(words)):
+        wps = measure(func, count)
+        logger.info("    %-50s %0.0f words/sec %s", bench_name, wps, note)
+
+
+    # === run benchmarks:
+
+    show_info('morph.parse(w)', _run_nofreq)
+    show_info('morph.parse(w)', _run, '(considering word frequencies)', total_usages)
+
+    if morph._result_type is not None:
+        show_info('morph.word_is_known(w)', _run_word_is_known, count=len(words)*10)
+        show_info("[p.normal_form for p in morph.parse(w)]", _run_normal_form)
+        show_info("[p.normalized for p in morph.parse(w)]", _run_normalized)
+        show_info("[p.lexeme for p in morph.parse(w)]", _run_lexeme, count=len(words)/5)
+        show_info("[{'NOUN'} in p.tag for p in morph.parse(w)]", _run_is_noun)
+        show_info("[p.tag.POS == 'NOUN' for p in morph.parse(w)]", _run_is_noun2)
+
+    logger.info("")
+
 
 def bench_all(dict_path=None, repeats=5):
     """ Run all benchmarks """
@@ -95,4 +138,4 @@ def bench_all(dict_path=None, repeats=5):
     bench_parse(morph_plain, words, total_usages, repeats)
 
     end_time = datetime.datetime.now()
-    logger.info("\n----\nDone in %s.\n" % (end_time-start_time))
+    logger.info("----\nDone in %s.\n" % (end_time-start_time))
