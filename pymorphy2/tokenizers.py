b5ae95ba483a0ce73f146b59fa311deac42604c7
Mikhail Korobov <kmike84@gmail.com>
2015-04-18 01:16:08 +0500

    slightly faster tokenization

--- pymorphy2/tokenizers.py
+++ pymorphy2/tokenizers.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import re
+
 GROUPING_SPACE_REGEX = re.compile('([^\w_-]|[+])', re.U)
 
-def simple_word_tokenize(text):
-    """
-    Split text into tokens. Don't split by hyphen.
-    """
-    return [t for t in GROUPING_SPACE_REGEX.split(text)
-            if t and not t.isspace()]
+
+def simple_word_tokenize(text, _split=GROUPING_SPACE_REGEX.split):
+    """ Split text into tokens. Don't split by a hyphen. """
+    return [t for t in _split(text) if t and not t.isspace()]
