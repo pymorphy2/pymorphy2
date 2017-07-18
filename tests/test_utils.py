07b7c675810e5f3347bf3d459d9a5353813514f5
Mikhail Korobov <kmike84@gmail.com>
2017-07-18 14:55:14 +0500

    TST a less strict check
    pypy tests may sometimes use more than 500MB

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,4 +8,4 @@ from pymorphy2.utils import get_mem_usage
 def test_get_mem_usage():
     pytest.importorskip("psutil")
     rss = get_mem_usage()
-    assert 1000000 < rss < 500000000
+    assert 1000000 < rss < 1000000000  # 1MB to 1GB
